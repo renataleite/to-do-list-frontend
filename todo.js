7e43e1507bd7d1b31c9de16fe7ef7b5ff7668658
Renata Guedes <renataleitesg@gmail.com>
2021-01-19 21:47:33 +0000

    implement getTasks method

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
-new Vue({
+var app = new Vue({
     el: "#app",
     data: {
+        baseUrl: "https://morning-beach-24953.herokuapp.com",
         currentTask: "",
         todos: [
             { task: "Estudar", done: false },
@@ -8,8 +9,18 @@ new Vue({
             { task: "Limpar a casa", done: true }
         ]
     },
+    created: function () {
+        this.getTasks();
+    },
     methods: {
+        async getTasks() {
+            var resource = this.baseUrl + "/api/v1/todolist"
+            const response = await fetch(resource);
+            console.log(response);
+            this.todos = response.toDoList.data;
+        },
         addTask() {
+
             this.todos.push({
                 task: this.currentTask,
                 done: false
@@ -17,6 +28,7 @@ new Vue({
             this.currentTask = "";
             this.sortTasks();
         },
+        /* muda o estado “done” da tarefa passada como parâmetro*/
         toggleTask(todo) {
             todo.done = !todo.done;
             this.sortTasks();
@@ -24,8 +36,9 @@ new Vue({
         delTask(todo) {
             this.todos = this.todos.filter(el => el.task !== todo.task);
         },
+        //responsável por ordenar a lista de tarefas
         sortTasks() {
-            this.todos.sort((a,b) => a.done - b.done);
+            this.todos.sort((a, b) => a.done - b.done);
         },
         isValidInput() {
             return !(!this.currentTask.trim() || this.checkIfTodoExists());
@@ -34,6 +47,7 @@ new Vue({
             return this.todos.some((todo) => todo.task === this.currentTask.trim());
         }
     },
+    /*cachear e executar métodos somente quando algum dado reativo for alterado e/ou em métodos complexos com elevado tempo de execução, em prol da otimização. */
     computed: {
         filteredTasks() {
             return this.todos.filter(
